fe57c5acec8f2de8c9e4d70da0ff60a04377518e
Mahti <mahtishavaev@gmail.com>
2021-04-03 05:51:39 +0300

    feat: implement fetching weather by place id

--- routes/weather.js
+++ routes/weather.js
@@ -1,12 +1,30 @@
 const router = require("express").Router();
 const axios = require("axios");
+const Place = require("../models/Place");
 
 router.get("/", async (req, res, next) => {
   const url = process.env.WEATHER_API_URL;
   const apiKey = process.env.WEATHER_API_KEY;
+  const locationId = req.query.id;
+  let lat, lon, locationName;
+
+  if (!locationId) {
+    return res.status(400).json({ error: "City id is required" });
+  }
+
   try {
-    const { status, data } = await axios.get(url, { params: { ...req.query, appid: apiKey } });
-    res.status(status).json(data);
+    const location = await Place.findOne({ geoname_id: locationId }).exec();
+    lat = location.latitude;
+    lon = location.longitude;
+    locationName = location.name;
+  } catch (error) {
+    return res.status(404).json({ error: "Location not found" });
+  }
+
+  try {
+    const queryParams = { lat, lon, units: "metric", exclude: "hourly,minutely", appid: apiKey };
+    const { status, data } = await axios.get(url, { params: queryParams });
+    res.status(status).json({ ...data, location_name: locationName });
   } catch ({ response }) {
     res.status(response.status).json(response.data);
   }
